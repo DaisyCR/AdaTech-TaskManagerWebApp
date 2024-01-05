089032d2148fe0ece6f5f107224a72244c7fdafb
Daisy Reis <90834386+DaisyCR@users.noreply.github.com>
2024-01-05 05:56:19 -0300

    add assignments link to navbar

--- TaskManager/Models/Assignment.cs
+++ TaskManager/Models/Assignment.cs
@@ -5,14 +5,13 @@ namespace TaskManager.Models
     public class Assignment
     {
         public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public Status Status { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime EndDate { get; set; }
-        public Status Status { get; set; }
         //public dynamic? Responsible { get; set; } //TODO create user
-
-        public int ProjectId { get; set; }
 
         public Assignment()
         {
