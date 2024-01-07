4035b30a322cb706b523e2043b5d5945c0e1f480
Daisy Reis <90834386+DaisyCR@users.noreply.github.com>
2024-01-07 15:23:24 -0300

    make endDate and Status optional

--- TaskManager/Models/Assignment.cs
+++ TaskManager/Models/Assignment.cs
@@ -6,11 +6,11 @@ namespace TaskManager.Models
     {
         public int Id { get; set; }
         public int ProjectId { get; set; }
-        public Status Status { get; set; }
+        public Status? Status { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime CreationDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
         //public dynamic? Responsible { get; set; } //TODO create user
 
         public Assignment()
